9b025ae5674b984f6c3b662d3adde8b5f9987d85
Bernardo Gonçalves <140979843+BernardoGoncalvess@users.noreply.github.com>
2025-08-17 21:04:36 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -9,8 +9,7 @@ const hint = window.document.getElementById("hint");
 // Logic
 
 if (startScreen.classList.contains("active")) {
-  //If start screen have the class 'active'
-  // Hide main game page
+  // Hide main game page when the app is loaded
   mainGame.style.display = "none";
 }
 
@@ -26,7 +25,42 @@ playButton.addEventListener("click", function () {
     startScreen.style.display = "none";
     mainGame.style.display = "block";
 
-    let mysterynumber = Math.floor(Math.random() * 101);
+    let mysterynumber = Math.floor(Math.random() * 101); // Generate a random number between 1 - 100
     console.log(mysterynumber);
+
+    // innit
+
+    playerGuessBtn.addEventListener("click", function () {
+      // Register player input on the 'guess' button
+      let guess = parseInt(playerGuess.value); // Turn the player guess input into a integer
+
+      if (guess > 100) {
+        alert("Must be under 100");
+      } else if (guess === mysterynumber) {
+        // If the player guessed correctly
+        const line = window.document.createElement("p"); // Create a 'p' element
+        line.classList.add("answer"); // Give it the class 'answer'
+        line.textContent = "🎉 YOU GUESSED IT, CONGRATS! 🎉"; // Say that it is corret
+        hint.appendChild(line); // Show the line on 'hint' div
+
+        playerGuess.value = "";
+      } else if (guess < mysterynumber) {
+        hint.value = "";
+        const line = window.document.createElement("p"); // Create a 'p' element
+        line.classList.add("answer"); // Give it the class 'answer'
+        line.textContent = "😕 You're a little off, try a bigger number ⬆️"; // Say to try a bigger number
+        hint.appendChild(line); // Show the line on 'hint' div
+
+        playerGuess.value = "";
+      } else if (guess > mysterynumber) {
+        hint.value = "";
+        const line = window.document.createElement("p"); // Create a 'p' element
+        line.classList.add("answer"); // Give it the class 'answer'
+        line.textContent = "😕 You're a little off, try a smaller number ⬇️"; // Say to try a smaller number
+        hint.appendChild(line); // Show the line on 'hint' div
+
+        playerGuess.value = "";
+      }
+    });
   }
 });
