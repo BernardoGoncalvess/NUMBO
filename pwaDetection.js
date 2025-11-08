be405ad869ad73513a67f70719c068e192940aa4
Bernardo Gonçalves <mrbenns16@gmail.com>
2025-11-08 17:34:27 +0000

    hotfix(): Fixed the bug that made the non pwa massage show on the pwa app

--- pwaDetection.js
+++ pwaDetection.js
@@ -1,15 +1,18 @@
 // Detect if it's in PWA on phone
 
 // function that detects if it's opened in standalone ----------
-function isPwa() {
-  return (
-    window.matchMedia('("display-mode: standalone")').matches ||
-    window.navigator.standalone === true //IOS
-  );
-}
+// function isPwa() {
+//   return (
+//     window.matchMedia('("display-mode: standalone")').matches ||
+//     window.navigator.standalone === true //IOS
+//   );
+// }
 
 // init if its opened in standalone ----------
-if (isPwa()) {
+if (
+  window.matchMedia('("display-mode: standalone")').matches ||
+  window.navigator.standalone === true
+) {
   console.log("PWA-active");
 
   window.document.getElementById("non-pwa").style.display = "none !important";
