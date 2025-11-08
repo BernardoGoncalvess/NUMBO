46f59f0399fd712c50bc88d5dc523aefc2efdb5e
Bernardo Gonçalves <mrbenns16@gmail.com>
2025-11-08 17:24:15 +0000

    bugfix(): Fixed the bug that did't detected if it's PWA. Added mediaqueries to show messages to use it as pwa

--- pwaDetection.js
+++ pwaDetection.js
@@ -9,51 +9,12 @@ function isPwa() {
 }
 
 // init if its opened in standalone ----------
-// if (isPwa()) {
-//   console.log("PWA");
-// } else {
-//   // Hide elements
-//   startScreen.style.display = "none";
-//   footer.style.display = "none";
+if (isPwa()) {
+  console.log("PWA-active");
 
-//   const newElement = document.createElement("div"); // Create div
-//   newElement.setAttribute("id", "non-pwa"); // Give it the id 'non-pwa'
-
-//   const pMessage = document.createElement("p"); // Create a 'p' element to store the image
-//   pMessage.classList = "nonPwaPage"; // Give it the class 'nonPwaPage'
-//   pMessage.textContent = "You opened this app on a browser."; // Messsage to pass
-
-//   const pMessage2 = document.createElement("p"); // Create a 'p' element to store the image
-//   pMessage2.classList = "nonPwaPage"; // Give it the class 'nonPwaPage'
-//   pMessage2.textContent = "Press share and add the app to your homescreen."; // Messsage to pass
-
-//   newElement.appendChild(pMessage); // append the message to the div
-//   newElement.appendChild(pMessage2); // append the message to the div
-//   page.appendChild(newElement); // Append the div to the body
-// // }
-
-if (window.matchMedia("(width >= 480px)").matches) {
-  console.log("PC mode");
-} else if (window.matchMedia("(width <=. 480px").matches) {
-  console.log("Mobile mode");
-
-  startScreen.style.display = "none";
-  footer.style.display = "none";
-
-  const newElement = document.createElement("div"); // Create div
-  newElement.setAttribute("id", "non-pwa"); // Give it the id 'non-pwa'
-
-  const pMessage = document.createElement("p"); // Create a 'p' element to store the image
-  pMessage.classList = "nonPwaPage"; // Give it the class 'nonPwaPage'
-  pMessage.textContent = "You opened this app on a browser."; // Messsage to pass
-
-  const pMessage2 = document.createElement("p"); // Create a 'p' element to store the image
-  pMessage2.classList = "nonPwaPage"; // Give it the class 'nonPwaPage'
-  pMessage2.textContent = "Press share and add the app to your homescreen."; // Messsage to pass
-
-  newElement.appendChild(pMessage); // append the message to the div
-  newElement.appendChild(pMessage2); // append the message to the div
-  page.appendChild(newElement); // Append the div to the body
-} else if (isPwa()) {
-  console.log("Pwa mode");
+  window.document.getElementById("non-pwa").style.display = "none";
+  startScreen.style.display = "block";
+  footer.style.display = "block";
+} else {
+  console.log("PWA-not active");
 }
