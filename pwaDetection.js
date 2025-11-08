a8cb874fbe184029df68d866fb5a2239ec12bffd
Bernardo Gonçalves <mrbenns16@gmail.com>
2025-11-08 17:36:52 +0000

    hotfix(): Fixed the bug that made the non pwa massage show on the pwa app

--- pwaDetection.js
+++ pwaDetection.js
@@ -9,13 +9,14 @@
 // }
 
 // init if its opened in standalone ----------
+let nonPwa = window.document.getElementById("non-pwa");
 if (
   window.matchMedia('("display-mode: standalone")').matches ||
   window.navigator.standalone === true
 ) {
   console.log("PWA-active");
 
-  window.document.getElementById("non-pwa").style.display = "none";
+  nonPwa.style.display = "none";
   startScreen.style.display = "block";
   footer.style.display = "block";
 } else {
