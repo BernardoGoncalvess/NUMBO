acf314586e540996f366036561ca07ece7ce7f2c
Bernardo Gonçalves <mrbenns16@gmail.com>
2025-11-08 15:38:23 +0000

    refactor(): Made it pc compatible

--- pwaDetection.js
+++ pwaDetection.js
@@ -1,19 +1,42 @@
-// Detect if the app is open on the web or as a PWA
+// Detect if it's in PWA on phone
 
+// function that detects if it's opened in standalone ----------
 function isPwa() {
-  // function that detects if the link have /pwa included
   return (
     window.matchMedia('("display-mode: standalone")').matches ||
     window.navigator.standalone === true //IOS
   );
 }
 
-if (isPwa()) {
-  // If the display mode is standalone
-  console.log("ok");
-  // log 'ok'
-} else {
-  // Hide elements
+// init if its opened in standalone ----------
+// if (isPwa()) {
+//   console.log("PWA");
+// } else {
+//   // Hide elements
+//   startScreen.style.display = "none";
+//   footer.style.display = "none";
+
+//   const newElement = document.createElement("div"); // Create div
+//   newElement.setAttribute("id", "non-pwa"); // Give it the id 'non-pwa'
+
+//   const pMessage = document.createElement("p"); // Create a 'p' element to store the image
+//   pMessage.classList = "nonPwaPage"; // Give it the class 'nonPwaPage'
+//   pMessage.textContent = "You opened this app on a browser."; // Messsage to pass
+
+//   const pMessage2 = document.createElement("p"); // Create a 'p' element to store the image
+//   pMessage2.classList = "nonPwaPage"; // Give it the class 'nonPwaPage'
+//   pMessage2.textContent = "Press share and add the app to your homescreen."; // Messsage to pass
+
+//   newElement.appendChild(pMessage); // append the message to the div
+//   newElement.appendChild(pMessage2); // append the message to the div
+//   page.appendChild(newElement); // Append the div to the body
+// // }
+
+if (window.matchMedia("(width >= 480px)").matches) {
+  console.log("PC mode");
+} else if (window.matchMedia("(width <=. 480px").matches) {
+  console.log("Mobile mode");
+
   startScreen.style.display = "none";
   footer.style.display = "none";
 
@@ -31,4 +54,6 @@ if (isPwa()) {
   newElement.appendChild(pMessage); // append the message to the div
   newElement.appendChild(pMessage2); // append the message to the div
   page.appendChild(newElement); // Append the div to the body
+} else if (isPwa()) {
+  console.log("Pwa mode");
 }
